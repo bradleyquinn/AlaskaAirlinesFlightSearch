fbba780e9c571490fb4e83c83a513d6cfcedd998
Brad Quinn <bradleyquinn@live.com>
2017-12-12 15:23:30 -0800

    Comment and launch settings cleanup

--- AlaskaAirlinesWebAPI/Controllers/FlightsController.cs
+++ AlaskaAirlinesWebAPI/Controllers/FlightsController.cs
@@ -2,8 +2,6 @@
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Cors;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace AlaskaAirlinesWebAPI.Controllers
 {
 	[Route("api/[controller]")]
